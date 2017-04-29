29b6d21da13d204c9f29501a23fcb49c4a0d8e10
Matt Williams <mwilliams28291@googlemail.com>
2017-04-29 05:23:12 +0100

    Adding production demo API endpoint in place of local, and adding surge build script

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://damp-oasis-36206.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
